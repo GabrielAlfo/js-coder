44faa014963862cfa414e06ee1c02ca9d73919a1
Gabriel A <gagus07@gmail.com>
2024-07-18 23:31:49 -0300

    final

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+const cartItems = document.querySelector('.cart-items');
+const totalPriceElement = document.querySelector('.total-price');
+
+let cart = [];
+let totalPrice = 0;
+
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const product = {
+      name: button.previousElementSibling.previousElementSibling.textContent,
+      price: parseFloat(button.previousElementSibling.textContent.slice(1))
+    };
+
+    cart.push(product);
+    updateCart();
+  });
+});
+
+function removeFromCart(index) {
+  cart.splice(index, 1);
+  updateCart();
+}
+
+function updateCart() {
+  cartItems.innerHTML = '';
+  totalPrice = 0;
+  cart.forEach(product => {
+    const cartItem = document.createElement('div');
+    const cartbtn = document.createElement('button');
+    cartItem.textContent = `${product.name} - $${product.price}`;
+    cartItems.appendChild(cartItem);
+
+    totalPrice += product.price;
+  });
+
+  totalPriceElement.textContent = `Total: $${totalPrice.toFixed(2)}`;
+}
 
 const phones = [
     { name: "iPhone", price: 899 },
@@ -60,3 +98,40 @@ const phones = [
   if (orderNumber) {
     document.getElementById("orderNumber").textContent = `Número de Pedido: ${orderNumber}`;
   }
+  const b1 = document.getElementById("b1");
+  b1.addEventListener("click", () =>{
+    Swal.fire("Se añadio un procucto al carrito");
+  });
+  const b2 = document.getElementById("b2");
+  b2.addEventListener("click", () =>{
+    Swal.fire("Se añadio un procucto al carrito");
+  });
+  const b3 = document.getElementById("b3");
+  b3.addEventListener("click", () =>{
+    Swal.fire("Se añadio un procucto al carrito");
+  });
+  const b4 = document.getElementById("b4");
+  b4.addEventListener("click", () =>{
+    Swal.fire("Se añadio un procucto al carrito");
+  });
+  const b5 = document.getElementById("b5");
+  b5.addEventListener("click", () =>{
+    Swal.fire("Se añadio un procucto al carrito");
+  });
+
+  fetch('stock.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+
+    const stockDataElement = document.getElementById('stockData');
+
+    data.forEach(id => {
+      const itemElement = document.createElement('div');
+      itemElement.textContent = `Name: ${id.nombre}, Price: ${id.precio}`;
+      stockDataElement.appendChild(itemElement);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
